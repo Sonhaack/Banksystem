7ba44f43a5e22065b5688b474da3347e1bfb591e
magicaltermite <293106@VIAUC.dk>
2020-09-15 10:55:23 +0200

    Added start and close DB

--- src/Tier3/DatabaseServerImpl.java
+++ src/Tier3/DatabaseServerImpl.java
@@ -1,16 +1,36 @@
 package Tier3;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DatabaseServerImpl implements DatabaseServer
 {
 
+  private Connection c = null;
+
   @Override public void startDB()
   {
-
+    c = null;
+    try {
+      Class.forName("org.postgresql.Driver");
+      c = DriverManager
+          .getConnection("jdbc:postgresql://localhost:5432/Bank",
+              "postgres", "1234");
+      c.setAutoCommit(false);
+      System.out.println("Opened database successfully.");
+    } catch (ClassNotFoundException | SQLException e) {
+      e.printStackTrace();
+    }
   }
 
   @Override public void closeDB()
   {
-
+    try {
+      c.close();
+    } catch (SQLException throwables) {
+      throwables.printStackTrace();
+    }
   }
 
   @Override public void addCustomerToDB()
